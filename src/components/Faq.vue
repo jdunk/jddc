674d37790f14d9555164ff7f634f978241988c49
jdunk <jdunk@users.noreply.github.com>
2018-04-27 10:13:39 +0800

    Updated FAQ with a couple of images & links

--- src/components/Faq.vue
+++ src/components/Faq.vue
@@ -33,33 +33,48 @@
                             </template>
                             <template slot="answer">
                                 <p>
-                                Quite a bit, actually. The overarching theme is "picking up and solidifying new skills,"
-                                particularly the most interesting, fun, and useful ones which will pay dividends for
-                                years to come. Thus, I now have a very decent proficiency using Vue, Vuetify,
-                                Webpack, ES6, and authoring Chrome Extensions. I've learned how to create custom Amazon
-                                Alexa skills (using AWS Lambda) and beautiful &amp; fast charts with ChartJS.
+                                Quite a bit, actually. The overarching theme of my latest projects is "picking up and
+                                solidifying new skills," particularly the most interesting, fun, and useful ones which
+                                will pay dividends for years to come. Thus, I now have a very serviceable proficiency with
+                                Vue, Vuetify, Webpack, ES6, and Chrome Extension authoring. I've learned how to create
+                                custom Amazon Alexa skills (using AWS Lambda) and beautiful &amp; fast charts with
+                                ChartJS.
                                 </p>
 
                                 <p>
                                 In January, I devoured Investopedia in a week and taught myself everything one needs
-                                to know to be a proficient day-trader (and built a Laravel app to automate
-                                cryptocurrency day-trading on Binance).
+                                to know to be a proficient day-trader (and
+                                <a href="https://github.com/jdunk/trading-bot" target="_blank">built a Laravel app to automate
+                                cryptocurrency day-trading</a> on Binance).
                                 </p>
 
                                 <p>
                                 I've kept abreast of the latest versions of PHP and Laravel. I've attended Laravel
-                                meetups in San Francisco (where I once spoke directly with Taylor Otwell) and I've even
-                                presented at one in Manila.
+                                meetups in San Francisco (where I once spoke directly with Taylor Otwell)...<br>
+
+                                    <div class="img-center">
+                                        <a href="https://youtu.be/DuImStVSxEo?t=56m40s" target="_blank">
+                                            <img :src="require('@/assets/img/taylor-youtube-screenshot.png')"/>
+                                        </a>
+                                    </div>
+                                </p>
+
+                                <p>
+                                ...and I've even
+                                presented at one in Manila:
+                                    <div class="img-center">
+                                        <img :src="require('@/assets/img/clean-n-clear-code-video-screenshot.png')"/>
+                                    </div>
                                 </p>
 
                                 <p>
                                 And last but certainly not least, I've been able to accomplish all of this within the
-                                last few months by deeply caring about my personal health. I've been exercising daily,
+                                    last few months by deeply caring about my <strong>personal health</strong>. I've been exercising daily,
                                 have been highly mindful of my diet, and under no circumstances have I
                                 been sacrificing any sleep. I started swimming regularly again for the first time in
                                 over a decade. And I'm making sure that I regularly challenge my brain in completely new
                                 ways (to keep it in the best possible shape) by doing things such as learning
-                                Tagalog, some Japanese, and some crazy difficult Chopin songs on the piano.
+                                Tagalog and some Japanese, and some crazy-difficult Chopin songs on the piano.
                                 </p>
                             </template>
                         </Faq-item>
@@ -68,7 +83,8 @@
                                 What is your location exactly?
                             </template>
                             <template slot="answer">
-                                At this very moment, I'm in Manila. I was recently back in California for a few months.
+                                At this very moment, I'm in Manila, though I was recently back in California for a few
+                                months.
                                 The Philippines is my favorite place on Earth and I'd love to live here longer, but I
                                 also love to travel, ergo my location is flexible.
                             </template>
@@ -79,16 +95,16 @@
                             </template>
                             <template slot="answer">
                                 <p>
-                                It depends. First off, I'd be happy to relocate literally anywhere for a training
+                                It depends. First off, I'd be happy to relocate anywhere for a training
                                 period of 3 months or so.
                                 </p>
 
                                 <p>
-                                Now, if we're talking long-term (onsite only indefinitely), then we'd have to rule out SF and NYC
-                                (been there, done that, and these areas have many cultural and systemic problems that
-                                aren't worth the extra pay IMO, no offense). I'd love the chance to learn what it's
-                                like to live in the Midwest or in Austin or Colorado or the Southwest. Really any
-                                midsize area I think would be nice.
+                                Now, if we're talking long-term (onsite-only indefinitely), then we'd have to rule out
+                                SF and NYC (been there, done that, and while these areas have many nice things, they
+                                also have many cultural and systemic problems that aren't worth the extra pay IMO, no
+                                offense to anyone). I'd love the chance to learn what it's like to live in the Midwest
+                                or in Austin or Colorado or the Southwest. Really any midsize area I think would be nice.
                                 </p>
 
                                 <p>
@@ -129,12 +145,12 @@
                         </Faq-item>
                         <Faq-item>
                             <template slot="question">
-                                Would you be ok with living and working <strong>within the US</strong> only?
+                                Would you be ok with living and working anywhere <strong>within the US</strong> only?
                             </template>
                             <template slot="answer">
                                 Sure! There are many areas of the US I've never been to and would love to try out. The
                                 beauty of having remote freedom still applies here since I'm never "stuck" in any place
-                                I happen to not like. I can just swipe left and keep on truckin'. :)
+                                I end up not liking much. I can just swipe left and keep on truckin'. :)
                             </template>
                         </Faq-item>
                         <Faq-item>
@@ -197,10 +213,19 @@ export default {
 
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 
 p:last-child {
     margin-bottom: 0;
 }
 
+.img-center {
+    text-align: center;
+
+    img {
+        margin-left: auto;
+        margin-right: auto;
+        max-width: 100%;
+    }
+}
 </style>
