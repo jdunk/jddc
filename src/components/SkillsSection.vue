89cdb2b28150bc7faf66fc8fdd7ad4743bc37e3e
jdunk <jdunk@users.noreply.github.com>
2018-04-29 18:11:13 +0800

    Update Skills description

--- src/components/SkillsSection.vue
+++ src/components/SkillsSection.vue
@@ -13,16 +13,10 @@
                 My top skills are <strong>PHP, Laravel, JavaScript,</strong> and <strong>Vue.js</strong>.
             </p>
 
-            <p class="smaller">
-                (Virtually no project requires the usage of <em>only</em> our most favorite languages and tools.
-                However, there is indeed a major difference between a project that requires <em>some</em> tools
-                that aren't the greatest fit for a given developer, and one that requires <em>mostly</em> the usage of
-                such tools.)
-            </p>
-
             <p>
-                My hope is that the chart below can serve as a useful measuring stick for <strong>how good a fit</strong>
-                a given project would be for me.
+                I drew the following chart to illustrate both the <strong>skill level</strong> and <strong>interest level</strong> I have
+                in each given technology in the hopes that this would serve as a useful measuring stick for
+                <strong>how good a fit</strong> a given project would be for me.
             </p>
         </Section-description>
         <v-container
