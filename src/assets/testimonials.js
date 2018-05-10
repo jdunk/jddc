0f244c1cb465d9f83a02ab398accf56426ce5a12
jdunk <jdunk@users.noreply.github.com>
2018-05-10 10:03:54 +0800

    testimonials.js contents

---
+++ src/assets/testimonials.js
@@ -0,0 +1,34 @@
+module.exports = [
+    {
+        quote: `Each time I saw you at work, serious, quiet, and dedicated, I felt there was so much more
+                        hidden inside &mdash; a beautiful, deep, gentle and brave soul; not just a full-stack developer
+                        resource, but a ‘full-stack’ human.`,
+        by: 'Elena Gartshtein, engineering colleague at Schoology',
+    },
+    {
+        quote: `You’re a very goal-driven individual that excels independently and has a lot of drive into
+                        excelling in any project that comes your way.`,
+        by: 'Donovan Hughes',
+    },
+    {
+        quote: 'You’re good honestly and I believe you deserve 50usd per hr.',
+        by: 'Joe Palala, engineering colleague at iVolunteer',
+    },
+    {
+        quote: `You’re like super awesome; I look up to you [...] but that’s the truth, I’m inspired by how you
+                        are passionate with what you do. You make me want to do better [...] Know that there is someone
+                        who looks up to you and someday wants to be as good as you!`,
+        by: 'Rosselle, Junior Engineer at iVolunteer',
+    },
+    {
+        quote: `I evaluate the viability of startups for a living. I’ve met thousands of founders, and I can tell
+                        that you’ve got what it takes
+                        [to be a successful entrepreneur].`,
+        by: 'Yoon Choi, Investment Director at SAIC Capital',
+    },
+    {
+        quote: `
+            <a href="https://youtu.be/P315lfGH1RQ?t=15m38s" target="_blank">Guys, he's <em>good!</em></a>`,
+        by: 'Mikey Bustos, famous YouTube musician/comedian',
+    },
+];
