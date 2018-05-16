41421221b919b0983d6820a7e52d6784ddda5ba7
jdunk <jdunk@users.noreply.github.com>
2018-05-16 21:43:28 +0800

    Polyfill for iOS <= 10.2 issue: Object.values is undefined

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'ecma-proposal-object-values-entries/polyfill';
 import Vue from 'vue';
 import '@/assets/fonts.scss';
 import 'vuetify/dist/vuetify.min.css';
