af45b723f08aecbb9f2ae4b54cf87a35c9af22bc
jdunk <jdunk@users.noreply.github.com>
2024-11-03 15:43:20 +0000

    Fix issue: resume unlinked on new build release (add autolinking to build.js)

--- build/build.js
+++ build/build.js
@@ -8,6 +8,9 @@ const rm = require('rimraf')
 const path = require('path')
 const chalk = require('chalk')
 const webpack = require('webpack')
+
+const fs = require('fs')
+
 const config = require('../config')
 const webpackConfig = require('./webpack.prod.conf')
 
@@ -32,10 +35,15 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
       process.exit(1)
     }
 
-    console.log(chalk.cyan('  Build complete.\n'))
-    console.log(chalk.yellow(
-      '  Tip: built files are meant to be served over an HTTP server.\n' +
-      '  Opening index.html over file:// won\'t work.\n'
-    ))
+    fs.symlink('../../../resume', './dist/resume', err => {
+        if (err) throw err
+        console.log(chalk.yellow('Symlink to resume created'))
+
+        console.log(chalk.cyan('  Build complete.\n'))
+        console.log(chalk.yellow(
+          '  Tip: built files are meant to be served over an HTTP server.\n' +
+          '  Opening index.html over file:// won\'t work.\n'
+        ))
+    })
   })
 })
